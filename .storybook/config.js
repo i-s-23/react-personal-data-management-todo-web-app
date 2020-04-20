92821a019fea7891d36ebc2ce49eb872f0d9683a
ishida-s1 <I-s-23shun9268@gmail.com@users.noreply.github.com>
2020-04-20 12:05:03 +0900

    storybookの拡張機能を追加したがエラー発生

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,14 @@
-import { configure } from "@storybook/react";
+import { configure, addDecorator } from "@storybook/react";
+import { withInfo } from '@storybook/addon-info';
+import { withKnobs } from '@storybook/addon-knobs';
+import { withA11y } from '@storybook/addon-a11y';
+
+import requireContext from 'require-context.macro';
+
+addDecorator(withInfo);
+addDecorator(withKnobs);
+addDecorator(withA11y);
+
 
 // automatically import all files ending in *.stories.js
 configure(require.context("../stories", true, /\.stories\.tsx$/), module);
