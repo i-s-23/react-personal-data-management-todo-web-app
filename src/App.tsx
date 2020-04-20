65e6448fdc8feb8ab36d11835f853b5d8be60558
ishida-s1 <I-s-23shun9268@gmail.com@users.noreply.github.com>
2020-04-21 00:04:51 +0900

    storybook拡張機能の設定追記。gitignoreに静的ファイルを追記。

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 const App: React.FC = () => {
-  return (
-    <h1>START</h1>
-  )
+  return <h1>START</h1>;
 };
 
 export default App;
