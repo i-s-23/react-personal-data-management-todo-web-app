8c4f1b6ab5ee1d89ac1e06d4e80ab0e01294c415
I-s-23 <shun9268@egmail.com>
2020-04-22 03:35:44 +0900

    ペアプログラミングでModalの中身を実装。エラーの原因特定予定。余裕があればコンポーネントの粒度を小さくするよう改修する

--- src/component/ModalWindow/index.tsx
+++ src/component/ModalWindow/index.tsx
@@ -9,8 +9,8 @@ const customStyles = {
     right: "auto",
     bottom: "auto",
     marginRight: "-50%",
-    transform: "translate(-50%, -50%)",
-  },
+    transform: "translate(-50%, -50%)"
+  }
 };
 
 interface Props {
@@ -19,18 +19,92 @@ interface Props {
   handleModalConfirm: (value: boolean) => void;
 }
 
+export interface ModalFormVo {
+  name: string;
+  birthday: string;
+  height: number;
+  weight: number;
+}
+
 Modal.setAppElement("#root");
 const ModalWindow: React.FC<Props> = (props: Props) => {
+  const [modalForm, setModalForm] = useState<ModalFormVo | undefined>(
+    undefined
+  );
+
+  const updateForm = (part: Partial<ModalFormVo>): void => {
+    setModalForm({
+      ...modalForm,
+      ...part
+    });
+  };
+
+  const handleChangeName = (value: string): void => {
+    updateForm({ name: value });
+  };
+
+  const handleChangeBirthday = (value: string): void => {
+    updateForm({ birthday: value });
+  };
+
+  const handleChangeHeight = (value: number): void => {
+    updateForm({ height: value });
+  };
+
+  const handleChangeWeight = (value: number): void => {
+    updateForm({ weight: value });
+  };
+
+  const handleModalClose = (): void => {
+    props.handleModalClose(false);
+    setModalForm(undefined);
+  };
+
   return (
     <div>
       <Modal
         isOpen={props.modalIsOpen}
-        onRequestClose={(): void => props.handleModalClose(false)}
+        onRequestClose={(): void => handleModalClose()}
         style={customStyles}
         contentLabel="Example Modal"
       >
         <h2> ModalWindow </h2>
-        <InputLists />
+        <div>
+          <label>
+            名前
+            <input
+              value={modalForm?.name}
+              onChange={(e): void => handleChangeName(e.target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            生年月日
+            <input
+              value={modalForm?.birthday}
+              onChange={(e): void => handleChangeBirthday(e.target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            身長
+            <input
+              value={modalForm?.height}
+              onChange={(e): void => handleChangeHeight(e.target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            体重
+            <input
+              value={modalForm?.weight}
+              onChange={(e): void => handleChangeWeight(e.target.value)}
+            />
+          </label>
+        </div>
         <button
           type="button"
           onClick={(): void => props.handleModalConfirm(false)}
